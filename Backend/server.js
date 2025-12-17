2d92f81be0a4b8b3f865a62931d164337222e479
Purva shrivastva <shpurva6@gmail.com>
2025-12-17 11:12:38 +0530

    Managed the routes by using router

--- Backend/server.js
+++ Backend/server.js
@@ -1,20 +1,10 @@
 import express from "express";
-// const express = require("express");
+import notesRoutes from "./routes/notesRoutes.js";
 
-const app = express(); //Creates an Express application
-//here is the route
-app.get("/api/notes", (req, res) => {
-  res.send("you got the 5 notes");
-});
-app.post("/api/notes", (req, res) => {
-  res.status(201).json("Note created sucessfully");
-});
-app.put("/api/notes/:id", (req, res) => {
-  res.status(201).json("Note updated successfully");
-});
-app.delete("/api/notes/:id", (req, res) => {
-  res.status(300).json("Note deleted successfully");
-});
+const app = express(); //Creates an Express application , this the whole application
+
+app.use("/api/notes", notesRoutes);
+
 app.listen(5001, () => {
   //Start the server  ,Tells Node.js to listen on port 5001
   console.log("server stared on port : 5001");
