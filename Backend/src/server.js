6a29a03ea67e4811fd350ceeb37d9e812474d3f6
Purva shrivastva <shpurva6@gmail.com>
2026-01-22 16:28:59 +0530

    initial commit

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,9 +1,10 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
-import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
-
 dotenv.config();
 
 const app = express(); //Creates an Express application , this the whole application
@@ -12,6 +13,11 @@ const PORT = process.env.PORT || 5001;
 //middleware
 app.use(express.json()); //this middleware will parse JSON bodies: req.body
 app.use(rateLimiter);
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 // our simple custom middleware
 // app.use((req, res, next) => {
 //   console.log(`Req  method is ${req.method} & req url is ${req.url}`);
@@ -20,8 +26,11 @@ app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
 connectDB().then(() => {
-  app.listen(PORT, () => {
-    //Start the server  ,Tells Node.js to listen on port 5001
-    console.log("server stared on PORT :", PORT);
+  // app.listen(PORT, () => {
+  //   //Start the server  ,Tells Node.js to listen on port 5001
+  //   console.log("server stared on PORT :", PORT);
+  // });
+  app.listen(PORT, "0.0.0.0", () => {
+    console.log("server started on PORT :", PORT);
   });
 });
